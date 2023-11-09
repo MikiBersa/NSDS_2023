5cc52dabf0b2dce4f9bebfca971370f529751c3d
MikiBersa <trombettamiki@gmail.com>
2023-11-09 19:28:42 +0100

    TEMPLETE KAFKA

---
+++ LAB/KAFKA/src/main/java/it/polimi/middleware/kafka/admin/TopicManager.java
@@ -0,0 +1,48 @@
+package it.polimi.middleware.kafka.admin;
+
+import org.apache.kafka.clients.admin.*;
+
+import java.util.Collections;
+import java.util.Properties;
+import java.util.Set;
+
+public class TopicManager  {
+    private static final String defaultTopicName = "topicA";
+    // INVECE SE VOGLIO AVERE UN SOLO BROKER ALLORA VADO AD UTILIZZARE LE PARTIZIONI
+    // POSSO AVERE MULTIPLE CONSUMER NELLO STESSO GRUPPO CHE LEGGONO IN PARALLELO STESSO TOPIC MA IN PARTITIONS DIFFERENTI
+    // FARE UN PO' DI ESPERIMENTI
+    private static final int defaultTopicPartitions = 2;
+    // SE VOGLIO MULTIREPLICA DEVO FAR ANDARE PIù BROKER
+    private static final short defaultReplicationFactor = 1;
+
+    private static final String serverAddr = "localhost:9092";
+
+    public static void main(String[] args) throws Exception {
+        final String topicName = args.length >= 1 ? args[0] : defaultTopicName;
+        final int topicPartitions = args.length >= 2 ? Integer.parseInt(args[1]) : defaultTopicPartitions;
+        final short replicationFactor = args.length >= 3 ? Short.parseShort(args[2]) : defaultReplicationFactor;
+
+        Properties props = new Properties();
+        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, serverAddr);
+        AdminClient adminClient = AdminClient.create(props);
+
+        ListTopicsResult listResult = adminClient.listTopics();
+        Set<String> topicsNames = listResult.names().get();
+        System.out.println("Available topics: " + topicsNames);
+
+        if (topicsNames.contains(topicName)) {
+            System.out.println("Deleting topic " + topicName);
+            DeleteTopicsResult delResult = adminClient.deleteTopics(Collections.singletonList(topicName));
+            delResult.all().get();
+            System.out.println("Done!");
+            // Wait for the deletion
+            Thread.sleep(5000);
+        }
+
+        System.out.println("Adding topic " + topicName + " with " + topicPartitions + " partitions");
+        NewTopic newTopic = new NewTopic(topicName, topicPartitions, replicationFactor);
+        CreateTopicsResult createResult = adminClient.createTopics(Collections.singletonList(newTopic));
+        createResult.all().get();
+        System.out.println("Done!");
+    }
+}
