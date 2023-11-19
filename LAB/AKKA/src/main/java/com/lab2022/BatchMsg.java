db306bad0a862d6e1794b87db9bd3a8fc933b8c2
MikiBersa <trombettamiki@gmail.com>
2023-11-19 23:59:56 +0100

    AGGIUNTO LAB2022

---
+++ LAB/AKKA/src/main/java/com/lab2022/BatchMsg.java
@@ -0,0 +1,14 @@
+package com.lab2022;
+
+public class BatchMsg {
+	
+	private boolean isOn;
+	
+	public BatchMsg(boolean isOn) {
+		this.isOn = isOn;
+	}
+
+	public boolean isOn() {
+		return isOn;
+	}
+}
