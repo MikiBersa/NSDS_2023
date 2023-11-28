11ab6cc4ba7b097b48f883d97c8e5119afff512e
MikiBersa <trombettamiki@gmail.com>
2023-11-28 13:05:43 +0100

    SPARK

---
+++ LAB/SPARK/src/main/java/it/polimi/middleware/spark/batch/bank/Bank.java
@@ -0,0 +1,96 @@
+package it.polimi.middleware.spark.batch.bank;
+
+import static org.apache.spark.sql.functions.max;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+
+/**
+ * Bank example
+ *
+ * Input: csv files with list of deposits and withdrawals, having the following
+ * schema ("person: String, account: String, amount: Int)
+ *
+ * Queries
+ * Q1. Print the total amount of withdrawals for each person.
+ * Q2. Print the person with the maximum total amount of withdrawals
+ * Q3. Print all the accounts with a negative balance
+ */
+public class Bank {
+    private static final boolean useCache = true;
+
+    // QUI USIAMO SPARK SQL
+    public static void main(String[] args) {
+        final String master = args.length > 0 ? args[0] : "local[4]";
+        final String filePath = args.length > 1 ? args[1] : "./";
+        final String appName = useCache ? "BankWithCache" : "BankNoCache";
+
+        final SparkSession spark = SparkSession
+                .builder()
+                .master(master)
+                .appName(appName)
+                .getOrCreate();
+        spark.sparkContext().setLogLevel("ERROR");
+
+        final List<StructField> mySchemaFields = new ArrayList<>();
+
+        // VADO A CARICARE LO SCHEMA DELLA TABELLA
+        mySchemaFields.add(DataTypes.createStructField("person", DataTypes.StringType, true));
+        mySchemaFields.add(DataTypes.createStructField("account", DataTypes.StringType, true));
+        mySchemaFields.add(DataTypes.createStructField("amount", DataTypes.IntegerType, true));
+        final StructType mySchema = DataTypes.createStructType(mySchemaFields);
+
+        final Dataset<Row> deposits = spark
+                .read()
+                // se non un header ok, ma se l'ho carico direttamente lo schema
+                .option("header", "false")
+                .option("delimiter", ",")
+                .schema(mySchema)
+                // faccio la lettura del file con lo schema descritto sopra
+                .csv(filePath + "files/bank/deposits.csv");
+
+        final Dataset<Row> withdrawals = spark
+                .read()
+                .option("header", "false")
+                .option("delimiter", ",")
+                .schema(mySchema)
+                .csv(filePath + "files/bank/withdrawals.csv");
+
+        if(useCache){
+            withdrawals.cache();
+        }
+
+        // Q1. Total amount of withdrawals for each person
+
+        // MI RITORNA UNA TABELLA DI RIGHE PER OGNI PERSONA
+        // FACCIO LA QUERY CON LA STRUTTURA DI SPARK
+        final Dataset<Row> sumWithdrawals = withdrawals
+                .groupBy("person")
+                .sum("amount")
+                .select("person", "sum(amount)");
+
+        if(useCache){
+            withdrawals.cache();
+        }
+
+        sumWithdrawals.show();
+
+        // Q2. Person with the maximum total amount of withdrawals
+
+        // TODO
+
+        // Q3 Accounts with negative balance
+
+        // TODO
+
+        spark.close();
+
+    }
+}
---
+++ LAB/SPARK/src/main/java/it/polimi/middleware/spark/batch/iterative/InvestmentSimulator.java
@@ -0,0 +1,79 @@
+package it.polimi.middleware.spark.batch.iterative;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+/**
+ * Start from a dataset of investments. Each element is a Tuple2(amount_owned, interest_rate).
+ * At each iteration the new amount is (amount_owned * (1+interest_rate)).
+ *
+ * Implement an iterative algorithm that computes the new amount for each investment and stops
+ * when the overall amount overcomes 1000.
+ */
+public class InvestmentSimulator {
+    public static void main(String[] args) {
+        final String master = args.length > 0 ? args[0] : "local[1]";
+        final String filePath = args.length > 1 ? args[1] : "./";
+        final double threshold = 35;
+
+        final SparkConf conf = new SparkConf().setMaster(master).setAppName("InvestmentSimulator");
+        final JavaSparkContext sc = new JavaSparkContext(conf);
+        sc.setLogLevel("ERROR");
+
+        final JavaRDD<String> textFile = sc.textFile(filePath + "files/iterative/investment.txt");
+
+        int iteration = 0;
+
+        // ESTRAPOLO QUELLO CHE C'è DENTRO ALLA RIGA IN TUPLE
+        // <3, 0.01>
+        JavaRDD<Tuple2<Double, Double>> investements = textFile.map(w ->{
+           String[] values = w.split(" ");
+           double amountOwned = Double.parseDouble(values[0]);
+           double investmentRate = Double.parseDouble(values[1]);
+           return new Tuple2<>(amountOwned, investmentRate);
+        });
+
+        // sum è la somma di tutti gli investimenti del file
+        double sum = sumAmount(investements);
+
+        // PER FARE UNA ITERAZIONE DEVO CREARLO IO
+        // POI SPARK IDENTIFICA OGNI MAP / REDUCE PER INVIARLO AI VARI WORK -> TRASFORMAZIONI CREA NUOVI RDD => STAGE
+        // FA UNA FIVISIONE DEI VALORI
+        while (sum < threshold) {
+            iteration++;
+            // ricalcolo il valore del nuovo balance
+            // <10, 0.01>
+            // con i._2
+            // QUESTA è UNA MAP CHE NON VIENE ESEGUITA ORA NON CREA UN JOB
+            // siccome ci sono 5 righe => viene eseguito 5 volte per ogni iterazione
+            investements = investements.map(i -> {
+                // DA NOTARE CHE PER FARE QUESTA OPERAZIONE DEVE PRENDERE investements DELL'ULTIMA OPERAZIONE
+                // MA AD OGNI JOBS CANCELLA RDD CHE HO / PARTIAL RESULT E QUINDI RI FA DA CAMPO AD OGNI ITERAZIONE
+                // QUINDI PER SALVARE I SUM TRA JOBS BISOGNA USARE CACHE
+                System.out.println("AAA");
+                return new Tuple2<>(i._1 * (1 + i._2), i._2);
+            });
+            // QUINDI PER SALVARE I SUM TRA JOBS BISOGNA USARE CACHE
+            // INFATTI ULTIMO JOB NE FA 5 PERCHè RICORDA COSA HA FATTO PRIMA
+            investements.cache();
+            sum = sumAmount(investements);
+        }
+
+        System.out.println("Sum: " + sum + " after " + iteration + " iterations");
+        sc.close();
+    }
+
+
+    private static double sumAmount(JavaRDD<Tuple2<Double, Double>> investments){
+        // in investments ho tutte le tuple del map di prima riga 42 o 31
+        return investments.
+                mapToDouble(a -> a._1)
+                .sum();
+        // faccio una riduzione della somma -> STARTS JOB INTO SPARK
+        // java program aspetta il valore per proseguire per ogni reduce
+    }
+
+
+}
