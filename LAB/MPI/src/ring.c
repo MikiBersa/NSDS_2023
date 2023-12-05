eef55d8d9a4e0f4c663251c4cf45cdbd7069c316
MikiBersa <trombettamiki@gmail.com>
2023-12-05 19:38:59 +0100

    ESERCIZIO RING

--- LAB/MPI/src/ring.c
+++ LAB/MPI/src/ring.c
@@ -21,10 +21,23 @@ int main(int argc, char** argv) {
   int my_rank, world_size;
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  int other_rank = 1 - my_rank;
 
   int num_hops = 0;
+  for (int i=0; i<world_size; i++) {
+    int sender = i;
+    int receiver = (i+1) % world_size;
 
-  // TODO
+    // FARE UNA SOLUZIONE SCHEMATICA E SEMPLICE
+    if (my_rank == sender) {
+      num_hops++;
+      MPI_Send(&num_hops, 1, MPI_INT, receiver, 0, MPI_COMM_WORLD);
+      printf("Process %d sent msg with num hops = %d\n", my_rank, num_hops);
+    } else if (my_rank == receiver) {
+      MPI_Recv(&num_hops, 1, MPI_INT, sender, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      printf("Process %d received msg with num hops = %d\n", my_rank, num_hops);
+    }
+  }
 
   if (my_rank == 0) {
     printf("Final number of hops in process %d = %d\n", my_rank, num_hops);
