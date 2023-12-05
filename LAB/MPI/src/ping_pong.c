9185779faa73af7b4d361849edf869a9b96b1dfa
MikiBersa <trombettamiki@gmail.com>
2023-12-05 12:47:13 +0100

    ESERCIZIO PING

--- LAB/MPI/src/ping_pong.c
+++ LAB/MPI/src/ping_pong.c
@@ -2,19 +2,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Simple ping pong program to exemplify MPI_Send and MPI_Recs
+/*
+Write a ping-pong program
+– Process P0 sends a message to process P1 with a number
+– Process P1 replies and increments the number
+– Stop when the number overcomes a given threshold
+*/
 // Assume only two processes
 int main(int argc, char** argv) {
   const int tot_msgs = 100;
-  
+
   MPI_Init(NULL, NULL);
 
   int my_rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
   int num_msgs = 0;
+  int other_rank = 1 - my_rank;
 
-  // TODO
-  
+  /*
+  while(num_msgs < tot_msgs){
+    if(my_rank == 0){ // _> other_rank = 1
+      // mando il numero
+      MPI_Send(&num_msgs, 1, MPI_INT, other_rank, 0, MPI_COMM_WORLD);
+      // RICEVO IL MESSAGGIO
+      MPI_Recv(&num_msgs, 1, MPI_INT, other_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+      printf("Messaggio arrivato %d nel processo %d, da %d\n", num_msgs, my_rank, other_rank);
+
+      num_msgs ++;
+    }else{ // _> other_rank = 0
+      // mando il numero
+      MPI_Recv(&num_msgs, 1, MPI_INT, other_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      // RICEVO IL MESSAGGIO
+      MPI_Send(&num_msgs, 1, MPI_INT, other_rank, 0, MPI_COMM_WORLD);
+
+      printf("Messaggio arrivato %d nel processo %d, da %d\n", num_msgs, my_rank, other_rank);
+      num_msgs ++;
+    }
+  }*/
+
+
+  while (num_msgs < tot_msgs) {
+    // GIOCA SUL NUMERO PARI O DISPARI
+    if (num_msgs % 2 == my_rank) {
+      // INCREMENTA PRIMA
+      num_msgs++;
+      MPI_Send(&num_msgs, 1, MPI_INT, other_rank, 0, MPI_COMM_WORLD);
+      printf("Process %d sent message %d\n", my_rank, num_msgs);
+    } else {
+      MPI_Recv(&num_msgs, 1, MPI_INT, other_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      printf("Process %d received message %d\n", my_rank, num_msgs);
+    }
+  }
+
+
   MPI_Finalize();
 }
--- LAB/MPI/src/ring.c
+++ LAB/MPI/src/ring.c
@@ -5,6 +5,15 @@
 // Send a message in a ring starting from process 0 and increment by one at each hop.
 // When the message gets back to process 0, print the number of hops.
 
+/*
+Write a ring communication program
+– A set of processes exchange a token
+  • Process P0 sends the token to P1
+  • Process P1 sends the token to P2
+  ...
+– The program terminates after a given number of iterations over the ring
+*/
+
 // 1 -> 2 -> 3 -> ... -> finale ->  1
 int main(int argc, char** argv) {
   MPI_Init(NULL, NULL);
